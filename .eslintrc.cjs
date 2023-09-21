79c60fcd19ab257b5dfd2f1b1a0146f25724a865
BuShe Pie <bushe.cn@icloud.com>
2023-09-22 00:38:05 +0800

    doc: update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,17 @@
-// eslint-disable no-undef
-export const env = {
-  browser: true,
-  es2021: true,
-};
-
-export const extends = ["eslint:recommended", "plugin:vue/vue3-recommended"];
-
-export const parserOptions = {
-  ecmaVersion: "latest",
-  parser: "@typescript-eslint/parser",
-  sourceType: "module",
-};
-
-export const plugins = [
-  "vue",
-  "@typescript-eslint",
-];
+/* eslint-disable no-undef */
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": ["eslint:recommended",'plugin:vue/vue3-recommended'],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+}
